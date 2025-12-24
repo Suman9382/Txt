cadc3198c9406b51a195b5020e12304268ca32ec
BeNegative <gtgm4j@gmail.com>
2025-12-24 21:13:01 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,21 +3,22 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28578880"))
-API_HASH = environ.get("API_HASH", "5f8c87efde57e01d12c0ce98ffdf5928")
+API_ID = int(environ.get("API_ID", "39256507"))
+API_HASH = environ.get("API_HASH", "d9f88a9d3d207b8f6dfb08b762c30965")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "6814614245"))
-CREDIT = environ.get("CREDIT", "*MAHTO BHAI*")
+CREDIT = environ.get("CREDIT", "*YUS*")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6814614245').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7517872227').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6814614245').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7517872227').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
 
